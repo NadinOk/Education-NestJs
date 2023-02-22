3ae8139e8bb19bbd1544d709f52d4a4acb5d470d
Nadiia Onopriienko <nadiia@ok-shortcut.local>
2023-02-22 12:39:08 +0200

    add controllers and models

---
+++ src/test.ts
@@ -0,0 +1,22 @@
+// // {
+// //     "userId": 1,
+// //     "id": 1,
+// //     "title": "delectus aut autem",
+// //     "info": {
+// //     "desc": "delectus aut autem",
+// //         "isActive": true
+// // },
+// //     "tags": [
+// //     {
+// //         "name": "my name",
+// //         "value": 1000
+// //     }
+// // ]
+// // }
+//
+// interface Test {
+//     userId: number,
+//     id: number,
+//     title: string,
+//
+// }
