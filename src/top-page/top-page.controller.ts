ff4ac8e05e413be84bd52361468b09339a1d1edf
Nadiia Onopriienko <nadiia@ok-shortcut.local>
2023-02-22 14:41:43 +0200

    add connect to mongoose whith docker-compose

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,12 +1,12 @@
 import {Body, Controller, Delete, Get, HttpCode, Param, Patch, Post} from '@nestjs/common';
 import {TopPageModel} from "./top-page.model/top-page.model";
 import {FindTopPageDto} from "./dto/findTopPage.dto";
+import {ConfigService} from "@nestjs/config";
 
 @Controller('top-page')
 export class TopPageController {
     @Post('create')
     async create (@Body() dto: Omit<TopPageModel, '_id'>){
-
     }
 
     @Get(':id')
